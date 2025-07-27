11257327e5bba0ca766307dc10c0f407cf943a71
richurichu <paulsonrichu@gmail.com>
2025-07-28 00:21:01 +0530

    products page commented out

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -6,11 +6,11 @@ import ProductGrid from '@/components/products/ProductGrid'
 export default function ProductsPage() {
   return (
     <main>
-      <TitleBanner />
+      {/* <TitleBanner />
       <div className="max-w-7xl mx-auto px-4 py-16 grid grid-cols-1 md:grid-cols-4 gap-10">
         <Sidebar />
         <ProductGrid />
-      </div>
+      </div> */}
     </main>
   )
 }
